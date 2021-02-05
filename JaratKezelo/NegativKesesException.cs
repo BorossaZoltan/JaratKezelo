e3c654107fec80c2430e0486e675c0ce121fc6ef
Zoltán Borossa <borossa.zoltan@petrik.hu>
2021-02-05 12:43:53 +0100

    Negativ késés Exception megírása

---
+++ JaratKezelo/NegativKesesException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaratKezelo
+{
+    class NegativKesesException : Exception
+    {
+        public NegativKesesException(string jaratszam, int keses) 
+            : base($"Nem lehet negatív a késés szám! {jaratszam} :  késés - ({keses})")
+        {
+        }
+    }
+}
